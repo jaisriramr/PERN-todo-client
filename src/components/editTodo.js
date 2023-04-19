fbbecdd44950459aecb0898d768a2d5a278c90b9
Jaisriram <jaisriram.webdeveloper@gmail.com>
2023-04-19 11:55:55 +0530

    first commit

---
+++ src/components/editTodo.js
@@ -0,0 +1,66 @@
+import React, { Fragment, useState } from "react";
+
+const EditTodo = ({ todo }) => {
+    console.log(todo)
+    const [description, setDescription] = useState(todo.description)
+
+    const updateDescription = async (e) => {
+        e.preventDefault();
+        try {
+
+            let response = await fetch(`http://localhost:5000/todos/${todo.todo_id}`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    description: description
+                })
+            })
+
+            window.location = "/"
+
+        }catch(err) {
+            console.error(err);
+        }
+    }
+
+  return (
+    <Fragment>
+      <button
+        type="button"
+        className="btn btn-primary"
+        data-toggle="modal"
+        data-target={`#id${todo.todo_id}`}
+      >
+        Edit
+      </button>
+
+      <div className="modal" id={`id${todo.todo_id}`}>
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h4 className="modal-title">Edit Todo</h4>
+              <button type="button" className="close" data-dismiss="modal">
+                &times;
+              </button>
+            </div>
+            <div className="modal-body">
+                <input type="text" className="form-control" value={description} onChange={e => setDescription(e.target.value)} />
+            </div>
+            <div className="modal-footer">
+            <button type="button" className="btn btn-warning" onClick={e => updateDescription(e)} data-dismiss="modal">
+                Edit
+              </button>
+              <button type="button" className="btn btn-danger" data-dismiss="modal">
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default EditTodo;
